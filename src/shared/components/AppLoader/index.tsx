e7c1b1ddd5d90212a95740ffd3720976a2040e3e
Jhon Herrera <estebanhz9919@gmail.com>
2024-09-05 09:57:24 -0500

    [feat]: App Loader mejorado

--- src/shared/components/AppLoader/index.tsx
+++ src/shared/components/AppLoader/index.tsx
@@ -1,7 +1,12 @@
 const AppLoader = () => {
   return (
-    <div className="h-[100vh] bg-gray-400 flex justify-center items-center">
-      <h1 className="text-center text-white text-6xl">Cargando...</h1>
+    <div className="h-[100vh] bg-gray-300 flex justify-center items-center">
+      <div className="flex items-center justify-center min-h-screen bg-gray-300">
+        <div className="relative">
+          <div className="w-[90px] h-[90px] border-t-8 border-b-8 border-white rounded-full animate-spin"></div>
+          <div className="absolute inset-0 flex items-center justify-center"></div>
+        </div>
+      </div>
     </div>
   );
 };
